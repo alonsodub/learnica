e03e0c76febd1498fe9a02e6a4d3c7c64a5b93f2
Miguel Alonso <miguel.alonso@mobica.com>
2016-06-21 15:15:06 -0500

    (alonso) data class

---
+++ src/main/java/com/mobica/dev/training/learnica/domain/Data.java
@@ -0,0 +1,19 @@
+package com.mobica.dev.training.lernica.domain;
+
+
+public class Data
+{
+   //Any number of final, static fields
+   String name;
+   String skillId;
+   String value;
+
+   //Any number of abstract method declarations\
+
+   public String getName();
+   public void setName();
+   
+   public String getSkillId();
+   public void setSkillId();
+
+}
